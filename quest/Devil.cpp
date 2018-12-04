8ecf10ff88813f3e15b70f79f00d076147ac758e
Kyohei Oka <0622okakyo@gmail.com>
2018-12-04 14:12:16 +0900

    '練習中'

---
+++ quest/Devil.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include"header/devil.hpp"
+
+
+using namespace std;
+
+void Devil::Attacked(const int atk,const int hit){
+
+	if(((rand()%100+rand()%100)/2)<hit){
+		int damage=((hp-atk)<0)? hp:atk;
+		cout<<"Hit! –‚‰¤‚É"<<damage<<"‚Ìƒ_ƒ[ƒW"<<endl;
+		hp-=damage;}
+	else cout<<"Miss"<<endl;
+
+	if(!hp)cout<<"–‚‰¤‚Í“|‚³‚ê‚½"<<endl;}
