0f07e2eff3ab91ded939ed5a7c06b06c08527795
Kyohei Oka <0622okakyo@gmail.com>
2018-12-10 13:36:41 +0900

    '練習中'

--- SLAM/slamlauncher.hpp
+++ SLAM/slamlauncher.hpp
@@ -1,10 +1,53 @@
 class SlamLauncher{
-	private:
-		int startN;
-		int deawSkip;
-		bool odometryOnly;
+private:
+	int startN;
+	int deawSkip;
+	bool odometryOnly;
 
-		Pose2D lidaeOffset;
-		
+	Pose2D lidaeOffset;
+
+	SensorDataReader sreader;
+	PointCloudMap *pcamp;
+	MapDrawer mdrawer;
+	Frameworkcustmizer fcustom;
+
+public:
+	SlamLauncher() :startN(0),drawskip(10), odometryOnly(false),pcmap(nullptr){}
+
+	~SlamLauncher(){}
+
+	void  setStartN(int n){startN=n;}
+	void setOdometryOnly(bool p){odometryOnly=p;}
 
 };
+
+void SlamLauncher::run(){
+	mdrawer.initGunplot();
+	mdrawer.setAspectRatio(-0.9);
+
+	size_t cnt=0;
+	if(startN>0) skipData(startN);
+
+	Scan2D scan;
+
+	bool eof=sreader.loadScan(cnt,scan);
+	while (!eof){
+		if(odomtryOnly) mapByOdometry(&scan);
+		else efront.process(scan);
+			if(cnt%drawSkip==0) mdrawer.drawMapGp(*pcamp);
+		++cnt;
+		eof=sreader.loadScan(cnt,scan);
+	}
+	sreader.closeScanFile();
+
+	while (true){
+		Sleep(1000);
+	}
+}
+
+void SlamLauncher::skipData(int num){
+	Scan2D scan;
+
+	bool eof=sreader.loadScan(0,scan);
+
+}
